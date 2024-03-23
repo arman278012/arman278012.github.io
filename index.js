f9f71eae53a27f7a4e590d08e23a1cc979f68f44
arman278012 <aa278012@gmail.com>
2024-03-23 16:05:45 +0530

    resume updated

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-// const header = document.querySelector("header");
-
-// window.addEventListener("scroll", function () {
-//   header.classList.toggle("sticky", this.window.scrollY > 100);
-// });
-
-// let menu = document.querySelector("#menu-icon");
-// let nav = document.querySelector(".navlist");
-
-// menu.onclick = () => {
-//   menu.classList.toggle("bx-x");
-//   nav.classList.toggle("open");
-// };
-
-// window.onscroll = () => {
-//   menu.classList.remove("bx-x");
-//   nav.classList.remove("open");
-// };
-// const sr = ScrollReveal({
-//   distance: "25px",
-//   duration: 2500,
-//   reset: true,
-// });
-
-// sr.reveal(".home-text", { delay: 190, origin: "bottom" });
-
-// sr.reveal("#about,#skills,#githubs,#projects,#contact", {
-//   delay: 200,
-//   origin: "bottom",
-// });
-
 
 // *=============================== toggle icon navbar ==================================
 
@@ -55,8 +24,7 @@ let resume2 = document.getElementById("resume-button-2").addEventListener("click
 
 function NewTab() {
     window.open(
-        "https://drive.google.com/file/d/1EShcl5jORVYYNlLVtXxbpaITOZVesgc_/view?usp=drive_link",
-        "_blank"
+        "https://drive.google.com/drive/folders/1MRiz4p0kFT33dlLuDtZKfiR3xm_aZxE6?usp=sharing"
     );
 }
 
